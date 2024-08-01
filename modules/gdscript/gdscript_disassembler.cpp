82c58e014ffdf60e6084e5d129d37e240209c07f
LinuxUserGD <hugegameartgd@gmail.com>
2024-08-01 23:29:11 +0200

    update

--- modules/gdscript/gdscript_disassembler.cpp
+++ modules/gdscript/gdscript_disassembler.cpp
@@ -1190,11 +1190,11 @@ void GDScriptFunction::disassemble(const Vector<String> &p_code_lines) const {
 String GDScriptFunction::signature() const {
 	const MethodInfo &mi = get_method_info();
 	String signature = mi.name + "(";
-	for (int i = 0; i < mi.arguments.size(); i++) {
-		if (i > 0) {
+	for (List<PropertyInfo>::ConstIterator arg_itr = mi.arguments.begin(); arg_itr != mi.arguments.end(); ++arg_itr) {
+		if (arg_itr != mi.arguments.begin()) {
 			signature += ", ";
 		}
-		signature += mi.arguments[i].name;
+		signature += arg_itr->name;
 	}
 	signature += ")";
 	return signature;
