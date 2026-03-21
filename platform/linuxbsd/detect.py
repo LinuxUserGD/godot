ca0aa7decf4c1ff02a5cd699b168b081c3f32694
LinuxUserGD <hugegameartgd@gmail.com>
2026-03-21 18:22:28 +0000

    detect.py: remove openxr scons option

--- platform/linuxbsd/detect.py
+++ platform/linuxbsd/detect.py
@@ -340,7 +340,7 @@ def configure(env: "SConsEnvironment"):
         # No pkgconfig file so far, hardcode expected lib name.
         env.Append(LIBS=["embree4"])
 
-    if env["openxr"] and not env["builtin_openxr"]:
+    if not env["builtin_openxr"]:
         env.ParseConfig("pkg-config openxr --cflags --libs")
 
     if env["fontconfig"]:
